ac2d3e4a68067d4d6b6571c2b271a8250d94792a
adrianrevilla009 <adrianrg1996@gmail.com>
2022-12-23 16:28:58 +0100

    server start point

---
+++ server/src/main/java/es/codeurjc/eoloplanner/server/repository/EoloPlantRepository.java
@@ -0,0 +1,9 @@
+package es.codeurjc.eoloplanner.server.repository;
+
+
+import es.codeurjc.eoloplanner.server.model.EoloPlant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EoloPlantRepository extends JpaRepository<EoloPlant, Long> {
+
+}
